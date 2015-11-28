694c7164df101497f540f8a6a2ea954994ff45dc
doomy <vladimir.bartek@gmail.com>
2015-11-28 19:24:45 +0100

    Refactor directive handling

--- lib/Template/Directive/DirectiveFactory.php
+++ lib/Template/Directive/DirectiveFactory.php
@@ -7,12 +7,23 @@ use Template\Directive\IncludeDirective;
 
 class DirectiveFactory {
     public static function get_directive($directive_code) {
+        list($directive_name, $directive_parameters) = self::get_directive_data($directive_code);
+
+        switch($directive_name) {
+            case 'include':
+                $filename = $directive_parameters[0];
+                return new IncludeDirective($filename);
+                break;
+        }
+    }
+
+    private static function get_directive_data($directive_code) {
         $directive_code = trim($directive_code, '<');
         $directive_code = trim($directive_code, '>');
         $parts = explode("|", $directive_code);
-        if ($parts[0] == 'include') {
-            return new IncludeDirective($parts[1]);
-        }
+        $directive_name = array_shift($parts);
+        $directive_parameters = $parts;
+        return array($directive_name, $directive_parameters);
     }
 }
 
