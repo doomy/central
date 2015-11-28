4276626bc4e38835a237306d7124d37f11a2d54f
doomy <vladimir.bartek@gmail.com>
2015-11-28 19:17:43 +0100

    Change the character that separates directive parts

--- lib/Template/Directive/DirectiveFactory.php
+++ lib/Template/Directive/DirectiveFactory.php
@@ -9,7 +9,7 @@ class DirectiveFactory {
     public static function get_directive($directive_code) {
         $directive_code = trim($directive_code, '<');
         $directive_code = trim($directive_code, '>');
-        $parts = explode("=", $directive_code);
+        $parts = explode("|", $directive_code);
         if ($parts[0] == 'include') {
             return new IncludeDirective($parts[1]);
         }
