dcc32f99cc5a70fb288b9244942aeb3b7c7bfc5d
doomy <vladimir.bartek@gmail.com>
2015-12-01 19:25:09 +0100

    Implement simple else mechanism to the if directive

--- lib/Template/Directive/IfDirective.php
+++ lib/Template/Directive/IfDirective.php
@@ -3,14 +3,21 @@
 namespace Template\Directive;
 
 use Template;
+use Template\Directive\DirectiveParser;
 
 class IfDirective extends Directive {
     public function render($template_vars) {
         $output = "";
+        $else_separator = DirectiveParser::DIRECTIVE_START . 'else' . DirectiveParser::DIRECTIVE_END;
+        $parts = explode($else_separator, $this->contents);
+
         $condition = $this->parameters[0];
+        $template = new Template(null, $template_vars);
         if ($condition) {
-            $template = new Template(null, $template_vars);
-            $output = $template->process_output($this->contents);
+            $output = $template->process_output($parts[0]); // before else
+        }
+        else if (isset($parts[1])) {
+            $output = $template->process_output($parts[1]); // after else
         }
         return $output;
     }
