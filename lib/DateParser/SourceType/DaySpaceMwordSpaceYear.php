0772cf8ca76779129456d67eb97ed365a28168bc
doomy <vladimir.bartek@gmail.com>
2015-12-02 23:49:07 +0100

    Add source type DaySpaceMwordSpaceYear

--- lib/DateParser/SourceType/DaySpaceMwordSpaceYear.php
+++ lib/DateParser/SourceType/DaySpaceMwordSpaceYear.php
@@ -2,10 +2,26 @@
 
 namespace DateParser\SourceType;
 
+use DateParser;
+use DateParser\Certainty\Definite as DefiniteCertainty;
 
 class DaySpaceMwordSpaceYear extends SourceType {
     public function check() {
         $parts = explode(" ", $this->raw_date);
+        if (!DateParser::is_valid_day_number($parts[0])) return false;
+        if (!DateParser::is_month_name($parts[1])) return false;
+        if (!DateParser::is_valid_year_number($parts[2])) return false;
+
+        return true;
+    }
+
+    public function parse() {
+        $parts = explode(" ", $this->raw_date);
+
+        $this->day = $parts[0];
+        $this->month = DateParser::get_month_number($parts[1]);
+        $this->year = $parts[2];
+        $this->certainty = new DefiniteCertainty();
     }
 }
 
