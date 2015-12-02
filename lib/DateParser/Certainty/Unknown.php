44c13802c591137e39d68876db96b2f5ea2e0545
doomy <vladimir.bartek@gmail.com>
2015-12-02 19:10:09 +0100

    We don't need metod check() in Unknown certainty class

--- lib/DateParser/Certainty/Unknown.php
+++ lib/DateParser/Certainty/Unknown.php
@@ -7,10 +7,6 @@ use DateParser\Certainty\Certainty;
 
 class Unknown extends Certainty {
     protected $string_representation = 'Unknown';
-
-    public function check($raw_date) {
-        return true;
-    }
 }
 
 ?> 
