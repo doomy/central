767437ee3b176976648a2de6116047168e82c735
doomy <vladimir.bartek@gmail.com>
2015-12-05 20:40:56 +0100

    Handle ambigous keywords

--- lib/DateParser/SourceType/DayMonthNameYearWithMess.php
+++ lib/DateParser/SourceType/DayMonthNameYearWithMess.php
@@ -5,11 +5,17 @@ namespace DateParser\SourceType;
 use DateParser;
 use DateParser\SourceType\DayMonthnameYear;
 use DateParser\Certainty\Likely;
+use DateParser\Certainty\Ambiguous;
 
 
 class DayMonthNameYearWithMess extends DayMonthnameYear {
+    private $ambiguous = false;
+
     public function check() {
         if (!$this->separator) return false;
+        if (DateParser::contains_ambiguous_keyword($this->raw_date)) {
+            $this->ambiguous = true;
+        }
         $month_name = DateParser::contains_a_month_name($this->parts);
         if (!$month_name) return false;
         $this->parts = DateParser::strip_invalid_parts($this->parts);
@@ -18,7 +24,10 @@ class DayMonthNameYearWithMess extends DayMonthnameYear {
 
     public function parse() {
         parent::parse();
-        $this->certainty = new Likely();
+        if ($this->ambiguous)
+            $this->certainty = new Ambiguous();
+        else
+            $this->certainty = new Likely();
     }
 }
 
