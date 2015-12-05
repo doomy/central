84b946fb589ca1a3ba6f30f485f938193375a89e
doomy <vladimir.bartek@gmail.com>
2015-12-05 18:19:40 +0100

    Refactoring

--- lib/DateParser/SourceType/MonthnameDayYear.php
+++ lib/DateParser/SourceType/MonthnameDayYear.php
@@ -5,24 +5,24 @@ namespace DateParser\SourceType;
 use DateParser;
 use DateParser\Certainty\Definite as DefiniteCertainty;
 
-class MonthnameDayYear extends SourceType {
-    public function check() {
-        $parts = explode(" ", $this->raw_date);
-        if (count($parts) != 3) return false;
+class MonthnameDayYear extends SourceTypeWithSeparatorAndFixedParts {
+    protected $separator_subset = array(' ');
+    protected $expected_parts_amount = 3;
 
-        if (!DateParser::is_valid_day_number($parts[1])) return false;
-        if (!DateParser::is_month_name($parts[0])) return false;
-        if (!DateParser::is_valid_year_number($parts[2])) return false;
+    public function check() {
+        if (!parent::check()) return false;
+
+        if (!DateParser::is_valid_day_number($this->parts[1])) return false;
+        if (!DateParser::is_month_name($this->parts[0])) return false;
+        if (!DateParser::is_valid_year_number($this->parts[2])) return false;
 
         return true;
     }
 
     public function parse() {
-        $parts = explode(" ", $this->raw_date);
-
-        $this->day = $parts[1];
-        $this->month = DateParser::get_month_number($parts[0]);
-        $this->year = $parts[2];
+        $this->day = $this->parts[1];
+        $this->month = DateParser::get_month_number($this->parts[0]);
+        $this->year = $this->parts[2];
         $this->certainty = new DefiniteCertainty();
     }
 }
