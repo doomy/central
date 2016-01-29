29f180477a1b694efc9a319ea7a94f18ba3b93df
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-01-29 19:39:24 +0100

    Fix ComponentFactory to properly initialize template

--- lib/Component/ComponentFactory.php
+++ lib/Component/ComponentFactory.php
@@ -2,10 +2,12 @@
 
 namespace Component;
 
+use Template;
+
 class ComponentFactory {
 	public static function getComponent($componentClass) {
 		if($componentClass == Presenter::class) {
-			$template = new Template('index.tpl.php');
+			$template = new Template('index.tpl.php', array());
 			return new Presenter($template);
 		}
 	}
