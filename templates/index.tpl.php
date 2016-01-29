1c998d0994ef3193fafb85c863c8cf27ee36d99c
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-01-29 20:23:04 +0100

    Update presenter template

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01>
 <html>
-  <head></head>
-  <body>
-    presenter works
-  </body>
+	<head></head>
+	<body>
+		<<<render_component_children>>>
+	</body>
 </html>
