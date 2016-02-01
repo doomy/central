1a2b3c06ab13d19a369a22647cfb0a202d1fdebb
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-02-01 17:38:33 +0100

    Add presenter instance to base Controller

--- lib/base/Controller.php
+++ lib/base/Controller.php
@@ -2,13 +2,18 @@
 
 namespace base;
 
+use Component\Presenter;
+use Component\ComponentFactory;
+
 abstract class Controller {
     protected $action;
+	protected $presenter;
 
     public function __construct() {
         if (isset($_REQUEST['action'])) {
             $this->set_action($_REQUEST['action']);
         }
+		$this->presenter = ComponentFactory::getComponent(Presenter::class);
     }
     
     public function set_action($action) {
