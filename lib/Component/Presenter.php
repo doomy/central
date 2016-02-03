a2d03cd6ea0ff8ea6d8c43d5ebf9dbe02d7e25bd
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-02-03 14:25:43 +0100

    Auto indent presenter html output

--- lib/Component/Presenter.php
+++ lib/Component/Presenter.php
@@ -2,12 +2,20 @@
 
 namespace Component;
 
+use Gajus\Dindent\Indenter;
+
 class Presenter extends ContainerComponent {
     public $title;
 
-    function setTitle($title) {
+    public function setTitle($title) {
         $this->title = $title;
     }
+
+	public function render() {
+		$output = parent::render();
+		$indenter = new Indenter();
+		return $indenter->indent($output);
+	}
 }
 
 ?>
