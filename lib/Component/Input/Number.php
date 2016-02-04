9f5e2397e534a402afcd80a522715dd3e2756894
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-02-04 20:53:03 +0100

    Implement numeric input component

---
+++ lib/Component/Input/Number.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doomy
+ * Date: 4.2.16
+ * Time: 20:46
+ */
+
+namespace Component\Input;
+
+
+class Number extends TextInput {
+	protected $templateFileName = "component/input/numeric_input.tpl.php";
+}
---
+++ templates/component/input/numeric_input.tpl.php
@@ -0,0 +1,5 @@
+<<<if|$label&&$name>>><label for="$$name$$">$$label$$</label><<</if>>>
+<input type="number"
+	<<<if|$placeholder>>>placeholder="$$placeholder$$"<<</if>>>
+	<<<if|name>>>name="$$name$$"<<</if>>>
+/>
