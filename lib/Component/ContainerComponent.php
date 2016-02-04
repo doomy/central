e269de6e5f16df86a45a8a167f94b8640c7b7e4f
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-02-04 17:09:19 +0100

    Implement child search methods (by name, class)

--- lib/Component/ContainerComponent.php
+++ lib/Component/ContainerComponent.php
@@ -6,6 +6,10 @@ class ContainerComponent extends Component {
 	protected $children;
 	protected $templateFileName = "component/container.tpl.php";
 
+	public function __construct() {
+		parent::__construct();
+	}
+
 	public function setChildren(array $children) {
 		$this->children = $children;
 	}
@@ -16,6 +20,34 @@ class ContainerComponent extends Component {
 
 	public function getChildren() {
 		return $this->children;
+	}
+
+	public function hasChildren() {
+		return true;
+	}
+
+	public function getChildrenByClass($class) {
+		$children = array();
+		foreach($this->children as $child) {
+			if(get_class($child) == $class);
+			$children[] = $child;
+			if ($child->hasChildren()) {
+				$foundGrandChildren = $child->getChildrenByClass($class);
+				$children = array_merge($children, $foundGrandChildren);
+			}
+		}
+		return $children;
+	}
+
+	public function getChildByName($name) {
+		foreach($this->children as $child) {
+			if($child->name == $name) return $child;
+			if($child->hasChildren()) {
+				$child = $child->getChildByName($name);
+				if($child) return $child;
+			}
+		}
+		return false;
 	}
 }
 
