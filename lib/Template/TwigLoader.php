3a3d9687da93d9e5a103f67c303ff3c6e964f0ab
doomy <vladimir.bartek@gmail.com>
2016-02-17 20:58:16 +0100

    Look also into local path when looking for Twig templates

--- lib/Template/TwigLoader.php
+++ lib/Template/TwigLoader.php
@@ -11,9 +11,15 @@ class TwigLoader {
         if (!self::$twig) {
             $env = Environment::get_env();
             \Twig_Autoloader::register();
-            $templatePath = $env->CONFIG['CENTRAL_PATH'] . 'templates';
-            $cachePath    = $env->CONFIG['CENTRAL_PATH'] . 'cache/twig';
-            $loader = new \Twig_Loader_Filesystem($templatePath);
+
+            $localTemplatePath = $env->CONFIG['LOCAL_PATH'] . 'templates';
+            $localLoader = new \Twig_Loader_Filesystem($localTemplatePath);
+
+            $centralTemplatePath = $env->CONFIG['CENTRAL_PATH'] . 'templates';
+            $centralLoader = new \Twig_Loader_Filesystem($centralTemplatePath);
+
+            $loader = new \Twig_Loader_Chain(array($localLoader, $centralLoader));
+
             self::$twig = new \Twig_Environment($loader, array('cache' => false)); // TODO: use $cachePath. Currently it won't register template change for some reason
         }
         return self::$twig;
