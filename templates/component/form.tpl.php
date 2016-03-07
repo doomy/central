463b2e29eb67243b720c398255c4aa3bb6d81052
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-03-07 11:04:28 +0100

    Use POST as default form method

--- templates/component/form.tpl.php
+++ templates/component/form.tpl.php
@@ -1,4 +1,4 @@
-<form onsubmit="window.location.hash='';">
+<form onsubmit="window.location.hash='';" method="POST">
     <<<render_component_children>>>
     <input type="submit" value="send" />
     <a class="close" href="#">x</a>
