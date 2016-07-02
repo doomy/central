347cfd939246279922d8c509e44331c6a3bdf314
doomy <vladimir.bartek@gmail.com>
2016-07-02 20:20:15 +0200

    Add getMaxId to base model entity

--- lib/Model/Entity.php
+++ lib/Model/Entity.php
@@ -24,6 +24,12 @@ class Entity extends Model {
         $sql = "REPLACE INTO {$this->table} ($property_names_in) VALUES($values_in)";
         $this->mysqli->query($sql);
     }
+
+    public function getMaxId() {
+        $result = $this->mysqli->query("SELECT MAX(id) max_id FROM {$this->table}");
+        $row = $result->fetch_object();
+        return $row->max_id;
+    }
 }
 
 ?> 
