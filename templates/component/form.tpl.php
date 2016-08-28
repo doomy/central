e1fc9350b298dd84c5c8c0a17760ee97c86e0cb6
doomy <vladimir.bartek@gmail.com>
2016-08-28 14:03:35 +0200

    Close link shouldn't be in form template

--- templates/component/form.tpl.php
+++ templates/component/form.tpl.php
@@ -1,5 +1,5 @@
 <form onsubmit="window.location.hash='';" method="POST">
     <<<render_component_children>>>
     <input type="submit" value="send" />
-    <a class="close" href="#">x</a>
+    <!-- <a class="close" href="#">x</a>-->
 </form>
