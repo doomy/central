dcaf6fd416fd433e2dacb5f0fa9608c0b14d7505
doomy <vladimir.bartek@gmail.com>
2016-08-28 14:17:19 +0200

    Customizable submit button text

--- lib/Component/Form.php
+++ lib/Component/Form.php
@@ -4,6 +4,6 @@ namespace Component;
 use Component\ContainerComponent;
 
 class Form extends ContainerComponent {
+    public $submitText = 'send';
 	protected $templateFileName = 'component/form.tpl.php';
-
 }
--- templates/component/form.tpl.php
+++ templates/component/form.tpl.php
@@ -1,5 +1,5 @@
 <form onsubmit="window.location.hash='';" method="POST">
     <<<render_component_children>>>
-    <input type="submit" value="send" />
+    <input type="submit" value="$$submitText$$" />
     <!-- <a class="close" href="#">x</a>-->
 </form>
