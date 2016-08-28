6126ca643b234ebcf271367433c492e457bec168
doomy <vladimir.bartek@gmail.com>
2016-08-28 14:16:57 +0200

    Implement magic getters and setters in base Component class

--- lib/Component/Component.php
+++ lib/Component/Component.php
@@ -14,6 +14,18 @@ abstract class Component {
 
 	public function __construct() {}
 
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
+
+    public function __set($property, $value) {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+    }
+
 	public function assignTemplate(Template $template) {
 		$this->template = $template;
 	}
